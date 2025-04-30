cfd6823fcfd5906407bfb82284b03364e61e50d7
lilly <foofieowo@gmail.com>
2025-04-30 20:19:35 +0200

    Update of the base project.

---
+++ src/main/java/foofieowo/cheatmod/CheatMod.java
@@ -0,0 +1,12 @@
+package foofieowo.cheatmod;
+
+import necesse.engine.modLoader.annotations.ModEntry;
+
+@ModEntry
+public class CheatMod {
+
+    public void init() {
+        System.out.println("Hello world from my example mod!");
+    }
+
+}
